5b1985f1bb1574a8d28120ef41cbb793c81b7f0c
Mark Beale <bealebeaverst@gmail.com>
2024-07-05 14:35:53 -0700

    Victorious

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Mark Beale is the champion';
 // Write your code here!
